40545803bdadfddcf472601852f6916648169b44
adesyefiraa <102138207+adesyefiraa@users.noreply.github.com>
2023-01-19 12:48:17 +0700

    Add files via upload

---
+++ tambah_pengguna.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Biodata</title>
+</head>
+<body text="black">
+	<center>
+	<b>FORM TAMBAH DATA</b>
+	<form action="proses_tambah_data.php" method="POST">
+	<table border="1" bgcolor="#eeddd3"><br>
+		<tr>
+</body>
+			<td>
+				NAMA
+			</td>
+			<td>
+				<INPUT name="namaku" TYPE="TEXT" PLACEHOLDER="nama anda">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				PASSWORD
+			</td>
+			<td>
+				<INPUT name="katasandiku" TYPE="PASSWORD" PLACEHOLDER="password anda">
+			</td>
+		</tr>
+		<tr>
+			
+			
+			<td>
+				<INPUT  TYPE="submit" value="Kirim Data">
+			</td>
+		</tr>
+	</table>
+	</form>
+</center>
+</body>
+</html>
